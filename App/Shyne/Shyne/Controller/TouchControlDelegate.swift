110b1f353c78e5494af09f3e17baa8162b155c68
Victor Vasconcelos <victorabroum@gmail.com>
2018-10-17 15:49:35 -0400

    Add File, TouchControl Delegate

---
+++ App/Shyne/Shyne/Controller/TouchControlDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  TouchControlDelegate.swift
+//  Shyne
+//
+//  Created by Victor Vasconcelos on 17/10/18.
+//  Copyright © 2018 Abacate. All rights reserved.
+//
+
+import Foundation
+
+protocol TouchControlDelegate {
+    func follow(command: String?)
+}
--- App/Shyne/Shyne/Controller/TouchController.swift
+++ App/Shyne/Shyne/Controller/TouchController.swift
@@ -22,6 +22,7 @@ class TouchController: SKSpriteNode {
     }
     
     required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
         fatalError("init(coder:) has not been implemented")
     }
     
