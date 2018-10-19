04d8f77194938c86d92f626d30da2b0392fa712a
nickson kley <nicksonkley14@gmail.com>
2018-10-19 09:28:20 -0400

    apagando teste

--- App/Shyne/Shyne/Scenes/Main/GameScene.swift
+++ App/Shyne/Shyne/Scenes/Main/GameScene.swift
@@ -22,8 +22,6 @@ class GameScene: SKScene {
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
-    var balaodeescolha: baloesDeEscolha?
-    
     private var lastUpdateTime : TimeInterval = 0
     
     private var playerMovement: PlayerMovement = .idle
@@ -36,9 +34,6 @@ class GameScene: SKScene {
         
         // Cria a tree para cena
         // makeTreeOfRoom()
-        balaodeescolha = baloesDeEscolha(personagem: playerNode, direcao: .left, respostas: rootNode.choices)
-        
-        balaodeescolha!.desenhar()
         
     }
     
