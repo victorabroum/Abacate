130ed93e70435c22416bd59be3281c767753ee4c
Victor Vasconcelos <victorabroum@gmail.com>
2018-10-22 10:45:42 -0400

    Add and test background on Bakery

--- App/Shyne/Shyne/ViewController/GameViewController.swift
+++ App/Shyne/Shyne/ViewController/GameViewController.swift
@@ -17,10 +17,10 @@ class GameViewController: UIViewController {
         
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
-        if let scene = GKScene(fileNamed: "HouseScene03") {
+        if let scene = GKScene(fileNamed: "GameScene") {
             
             // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! HouseScene03? {
+            if let sceneNode = scene.rootNode as! GameScene? {
                 
                 // Copy gameplay related content over to the scene
                 // sceneNode.entities = scene.entities
