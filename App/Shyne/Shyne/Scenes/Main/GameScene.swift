7ec06616870c3e7614a1c20153cc03875ce1cf8f
nickson kley <nicksonkley14@gmail.com>
2018-10-22 16:37:37 -0400

    classe de dialogo ta daciolo

--- App/Shyne/Shyne/Scenes/Main/GameScene.swift
+++ App/Shyne/Shyne/Scenes/Main/GameScene.swift
@@ -12,62 +12,65 @@ import GameplayKit
 class GameScene: SKScene,SKPhysicsContactDelegate {
     
     private var playerNode: PlayerNode?
-    var caixa : caixaDeDialogo?
-    var escolhas: baloesDeEscolha?
-    var indexNode : Node?
-    var ballon = false
+    var dialogavel1: Dialogavel?
+//    var caixa : caixaDeDialogo?
+//    var escolhas: baloesDeEscolha?
+//    var indexNode : Node?
+//    var ballon = false
     
     override func sceneDidLoad() {
         
-        house01makeTree()
-        indexNode = house01Root
 
         self.playerNode = childNode(withName: "playerNode") as? PlayerNode
         
         physicsWorld.contactDelegate = self
         
+        dialogavel1 = Dialogavel(Playernode: playerNode!)
+        
+        house01makeTree()
+        dialogavel1!.indexNode = house01Root
         
     }
     
-    public func changeIndexNodeBallon(node: [Node]){
-        indexNode = node.first
-        
-    }
-    
-    public func drawnDialog(){
-        
-//        caixa = caixaDeDialogo(personagem: playerNode, texto: indexNode!.text, cena: self)
-//        if(indexNode!.childrens.first != nil){
+//    public func changeIndexNodeBallon(node: [Node]){
+//        indexNode = node.first
+//        
+//    }
+//    
+//    public func drawnDialog(){
+//        
+////        caixa = caixaDeDialogo(personagem: playerNode, texto: indexNode!.text, cena: self)
+////        if(indexNode!.childrens.first != nil){
+////            indexNode = indexNode!.childrens.first
+////        }
+////        caixa?.entrar()
+//        
+//        if(!ballon){
+//            if(indexNode != nil){
+//                self.playerNode!.playerCanWalk(false)
+//                if(escolhas != nil){
+//                    escolhas?.sair()
+//                }
+//                caixa = caixaDeDialogo(personagem: playerNode!, texto: indexNode!.text, cena: self)
+//                caixa?.entrar()
+//                if(!(indexNode!.choices.isEmpty)){
+//                    ballon = true
+//                    escolhas = baloesDeEscolha(personagem: playerNode!, respostas: indexNode!.choices, cena: self)
+//                }
+//                else{
+//                    indexNode = indexNode!.childrens.first
+//                }
+//            }
+//            else{
+//                self.playerNode!.playerCanWalk(true)
+//            }
+//        }
+//        else{
+//            escolhas?.desenhar()
+//            ballon = false
 //            indexNode = indexNode!.childrens.first
 //        }
-//        caixa?.entrar()
-        
-        if(!ballon){
-            if(indexNode != nil){
-                self.playerNode!.playerCanWalk(false)
-                if(escolhas != nil){
-                    escolhas?.sair()
-                }
-                caixa = caixaDeDialogo(personagem: playerNode!, texto: indexNode!.text, cena: self)
-                caixa?.entrar()
-                if(!(indexNode!.choices.isEmpty)){
-                    ballon = true
-                    escolhas = baloesDeEscolha(personagem: playerNode!, respostas: indexNode!.choices, cena: self)
-                }
-                else{
-                    indexNode = indexNode!.childrens.first
-                }
-            }
-            else{
-                self.playerNode!.playerCanWalk(true)
-            }
-        }
-        else{
-            escolhas?.desenhar()
-            ballon = false
-            indexNode = indexNode!.childrens.first
-        }
-    }
+//    }
     
     override func didMove(to view: SKView) {
         self.playerNode?.prepareControl(withCamera: camera!, inScene: self)
@@ -80,15 +83,15 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         
         if let nome=contact.bodyA.node?.name!{
-            caixa = caixaDeDialogo(personagem: contact.bodyA.node!, texto: (lista[nome]?.mensagem)!, cena: self)
+            dialogavel1!.caixa = caixaDeDialogo(personagem: contact.bodyA.node!, texto: (lista[nome]?.mensagem)!, dialogavel: dialogavel1!)
             lista["caixa"]?.funcaoEntrada = {(n:caixaDeDialogo)->Void in n.entrar()}
             lista["caixa"]?.funcaoSaida = {(n:caixaDeDialogo)->Void in n.sair()}
-            lista[nome]?.funcaoEntrada!(caixa!)
+            lista[nome]?.funcaoEntrada!(dialogavel1!.caixa!)
         }
     }
     func didEnd(_ contact: SKPhysicsContact) {
         if let nome=contact.bodyA.node?.name!{
-            lista[nome]?.funcaoSaida!(caixa!)
+            lista[nome]?.funcaoSaida!(dialogavel1!.caixa!)
         }
     }
 }
