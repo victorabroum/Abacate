f3075fbb7cd5985448a365d7910229a62541dbe5
ruyneto <neto3333@gmail.com>
2018-10-26 15:35:05 -0400

    Parede Ajeitada

--- App/Shyne/Shyne/Scenes/House/02(after bakery)/HouseScene02.swift
+++ App/Shyne/Shyne/Scenes/House/02(after bakery)/HouseScene02.swift
@@ -10,6 +10,24 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class HouseScene02: SKScene {
+class HouseScene02: SKScene,SKPhysicsContactDelegate {
+    var playerNode: PlayerNode?
+    var dialogBox01: Dialogavel?
+    
     var entities = [GKEntity]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    
+    override func sceneDidLoad() {
+        self.playerNode = childNode(withName: "playerNode") as? PlayerNode
+        
+        // Dizendo que a scene comanda o delegate
+        physicsWorld.contactDelegate = self
+        // Criando a box do diálogo
+        self.dialogBox01 = Dialogavel(cena: self)
+        //Preparando a tree story dessa scene
+        house01makeTree()
+        // Indicando a raiz da story
+        self.dialogBox01!.indexNode = house01Root
+    }
 }
