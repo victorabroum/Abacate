f85a9e410f79e7cb68e38626ac76da88bd1f2e9a
nickson kley <nicksonkley14@gmail.com>
2018-10-30 16:35:42 -0400

    room 2 only dialog

---
+++ App/Shyne/Shyne/Scenes/House/Room/Room02TreeStory.swift
@@ -0,0 +1,9 @@
+//
+//  Room02TreeStory.swift
+//  Shyne
+//
+//  Created by Nickson Kley Gonçalves Da Silva on 30/10/18.
+//  Copyright © 2018 Abacate. All rights reserved.
+//
+
+import Foundation
