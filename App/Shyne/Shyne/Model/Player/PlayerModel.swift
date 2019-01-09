5b91673199f3b04f8d857096ce59112998d60789
Unknown <neto3333@gmail.com>
2019-01-09 10:31:57 -0400

    PlayerModelComplete

--- App/Shyne/Shyne/Model/Player/PlayerModel.swift
+++ App/Shyne/Shyne/Model/Player/PlayerModel.swift
@@ -10,12 +10,12 @@ import Foundation
 
 class PlayerModel{
     
-    var Achviement:Set<String>
+    var achviement:Set<String>
     var keys:Set<String>
     var status:PlayerStatus?
     private static var player:PlayerModel?
     private init(){
-        Achviement = Set<String>(PlayerDAO.getAchviements())
+        achviement = Set<String>(PlayerDAO.getAchviements())
         keys = Set<String>(PlayerDAO.getKeys())
         status = PlayerDAO.getStatus()[0]
     }
@@ -25,7 +25,39 @@ class PlayerModel{
         }
         return player!
     }
+    static func incrementStatusBom(increment:Float){
+        getInstance().status?.bom+=increment
+    }
+    static func incrementStatusMedio(increment:Float){
+        getInstance().status?.medio+=increment
+    }
+    static func incrementStatusRuim(increment:Float){
+        getInstance().status?.ruim+=increment
+    }
+    static func addAchviement(ach:String){
+        getInstance().achviement.insert(ach)
+    }
+    static func addKeys(k:String){
+        getInstance().keys.insert(k)
+    }
+    static func savePlayer(){
+        //salvar status
+        PlayerDAO.updateStatus(status: self.getInstance().status!)
+        for a in getInstance().achviement{
+            if !PlayerDAO.getAchviements().contains(a){
+                PlayerDAO.addAchviement(key: a)
+            }
+        }
+        for k in getInstance().keys{
+            if !PlayerDAO.getKeys().contains(k){
+                PlayerDAO.addKey(key: k)
+            }
+        }
+        PlayerDAO.updateStatus(status: getInstance().status!)
+    }
+
 }
+
 struct PlayerStatus{
     var bom:Float
     var medio:Float
