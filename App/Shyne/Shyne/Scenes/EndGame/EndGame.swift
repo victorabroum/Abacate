e11e904d6c4494d26d320f6da86b6b4499d3b952
Victor Vasconcelos <victorabroum@gmail.com>
2019-01-10 14:43:05 -0400

    just save

--- App/Shyne/Shyne/Scenes/EndGame/EndGame.swift
+++ App/Shyne/Shyne/Scenes/EndGame/EndGame.swift
@@ -13,25 +13,59 @@ import SpriteKit
 class EndGame: SKScene {
 
     var labelNode: SKLabelNode?
+    var backgroundNode: SKSpriteNode?
     
     override func sceneDidLoad() {
         
         
         if let node = self.childNode(withName: "text") as? SKLabelNode {
+            print("TEXT")
             labelNode = node
-            labelNode?.text = NSLocalizedString("SUAS ESCOLHAS TE TROUXERAM ATÉ AQUI!", comment: "")
         }
         
-        let back = self.childNode(withName: "background") as! SKSpriteNode
-        if( happyStatus>normalStatus && happyStatus>shyStatus ){
-            back.texture = SKTexture(imageNamed: "final_bom")
+        if let back = self.childNode(withName: "background") as? SKSpriteNode{
+            backgroundNode = back
         }
-        else if( normalStatus>happyStatus && normalStatus>shyStatus ){
-            back.texture = SKTexture(imageNamed: "final_medio")
+        
+        labelNode?.horizontalAlignmentMode = .center
+        labelNode?.verticalAlignmentMode = .center
+        labelNode?.text = NSLocalizedString("SUAS ESCOLHAS\nTE TROUXERAM\nATÉ AQUI!", comment: "")
+        
+        labelNode?.run(SKAction.scale(by: 1.2, duration: 3.5)){
+            self.labelNode?.alpha = 0
+            self.backgroundNode?.texture = SKTexture(imageNamed: "Good_ending")
+            self.goodEnd()
         }
-        else{
-            back.texture = SKTexture(imageNamed: "final_ruim")
-        }
+        
+        
+        
+//        if( happyStatus>normalStatus && happyStatus>shyStatus ){
+//            back.texture = SKTexture(imageNamed: "final_bom")
+//        }
+//        else if( normalStatus>happyStatus && normalStatus>shyStatus ){
+//            back.texture = SKTexture(imageNamed: "final_medio")
+//        }
+//        else{
+//            back.texture = SKTexture(imageNamed: "final_ruim")
+//        }
+    }
+    
+    func goodEnd() {
+        
+        let felipeNode = SKSpriteNode(texture: SKTexture(imageNamed: "standing_01"), color: .clear
+            , size: CGSize(width: 138.28, height: 248))
+        felipeNode.position = CGPoint(x: -94.365, y: -49.329)
+        
+        let annaNode = SKSpriteNode(texture: SKTexture(imageNamed: "anna_standing_01"), color: .clear
+            , size: CGSize(width: 155.27, height: 285))
+        annaNode.position = CGPoint(x: 23.506, y: -34.5)
+        
+        self.addChild(felipeNode)
+        self.addChild(annaNode)
+        
+        felipeNode.run(SKAction(named: "felipe_standUp")!)
+        annaNode.run(SKAction(named: "anna_standUp")!)
+        
     }
 
 }
